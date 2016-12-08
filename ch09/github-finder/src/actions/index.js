c2f1cd00246e451e83988f7cf7371feb43f7181d
lyctea <396136337@qq.com>
2016-12-08 22:44:53 +0800

    new_code

--- ch09/github-finder/src/actions/index.js
+++
@@ -1,3 +0,0 @@
-//输出两个文件内的所有模块
-export * from './uiActions';
-export * from './githubActions';
--- ch09/github-finder/src/containers/ResultPageContainer/ResultPageContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import ResultPage from '../../components/ResultPage';
-
-export default connect(
-    (state) => ({
-        data: state.getIn(['github', 'data'])
-    }),
-    (dispatch) => ({})
-)(ResultPage);
